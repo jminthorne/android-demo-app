e8dccc9aa46974ec2b4acc3004b542adbda7b6dc
Rad Wadud <radwadud@ovpn-115-162.phx2.redhat.com>
2020-04-06 11:05:50 -0500

    Initial

---
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.app;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView mWebView;
+
+    @Override
+    @SuppressLint("SetJavaScriptEnabled")
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        mWebView = findViewById(R.id.activity_main_webview);
+        mWebView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        mWebView.loadUrl("http://route18gutpkm-mini-che.192.168.64.16.nip.io/php-web-simple/");
+        mWebView.setWebViewClient(new MyWebViewClient());
+
+        // LOCAL RESOURCE
+        // mWebView.loadUrl("http://routedbc2bjg5-mini-che.192.168.64.16.nip.io");
+
+        mWebView.setWebViewClient(new WebViewClient()
+        {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url)
+            {
+                view.loadUrl(url);
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
